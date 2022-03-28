463fa8ab8cafe5029d092817ba0e17f6d0064827
Edward Ji <jiziao6@gmail.com>
2022-03-28 11:53:20 +1100

    Fix sort fatal bugs

--- darray.c
+++ darray.c
@@ -271,37 +271,32 @@ int darray_unique(darray *arrp, comparator fp) {
 }
 
 void _swap_voidp(void **pp1, void **pp2) {
-    void *tempp = *pp1;
-    *pp1 = *pp2;
-    *pp2 = tempp;
+    void *temp = *pp1;
+	*pp1 = *pp2;
+	*pp2 = temp;
 }
 
-size_t partition(void **itempp, int pindex, int pivot, comparator fp) {
-    void *itemp = itempp[pivot];
-    size_t i = pindex - 1;
-    for (size_t j = pindex; j < pivot ; j++) {
-        if (fp(itempp[j], itemp) <= 0) {
-            i++;
-            _swap_voidp(itempp + i, itempp + j);
-        }
-    }
-    i++;
-    _swap_voidp(itempp[i], itempp[pivot]);
+ssize_t _partition(void **itempp, ssize_t low, ssize_t high, comparator cmp) {
+	void *pivot = itempp[high];
+	ssize_t i = low - 1;
 
-    return i;
+	for (ssize_t j = low; j < high; j++) {
+		if (cmp(itempp[j], pivot) < 0) {
+			i++;
+			_swap_voidp(itempp + i, itempp + j);
+		}
+	}
+	_swap_voidp(itempp + (i + 1), itempp + high);
+	return i + 1;
 }
 
-void _darray_qsort(void **itempp, size_t pindex, size_t pivot, comparator fp) {
-    if (pindex >= pivot) {
-        return;
-    }
+void _darray_qsort(void **itempp, ssize_t low, ssize_t high, comparator cmp) {
+	if (low < high) {
+		ssize_t pivot_i = _partition(itempp, low, high, cmp);
 
-    size_t t = (rand() % (pivot - pindex + 1) + pindex);
-    _swap_voidp(itempp[t], itempp[pivot]);
-
-    size_t q = partition(itempp, pindex, pivot, fp);
-    _darray_qsort(itempp, pindex, q - 1, fp);
-    _darray_qsort(itempp, q + 1, pivot, fp);
+		_darray_qsort(itempp, low, pivot_i - 1, cmp);
+		_darray_qsort(itempp, pivot_i + 1, high, cmp);
+	}
 }
 
 int darray_sort(darray *arrp, comparator fp) {
@@ -309,7 +304,9 @@ int darray_sort(darray *arrp, comparator fp) {
         return 0;
     }
 
-    _darray_qsort(arrp->itempp, 0, arrp->len - 1, fp);
+    if (arrp->len > 0) {
+        _darray_qsort(arrp->itempp, 0, arrp->len - 1, fp);
+    }
 
     return 1;
 }
